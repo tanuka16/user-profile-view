2a50fa37ebabf35bfe6b0adc831a629a8c570bbb
Tanuka16 <tanuka.das12@gmail.com>
2019-10-30 20:15:53 -0400

    added tab file

---
+++ src/components/tabs.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Tabs, Tab} from 'react-mdl';
+
+class Tab extends Component{
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+export default Tab;
