5dfbe3939a58854ce2596a110aa957950800c803
Tanuka16 <tanuka.das12@gmail.com>
2019-11-04 23:53:33 -0500

    updated

--- src/components/activeTabs.js
+++ src/components/activeTabs.js
@@ -1,41 +1,46 @@
 import React, {Component} from 'react';
 import {Tabs, Tab} from 'react-mdl';
 
-class ActiveTab extends Component{
-  state={
+class ActiveTabs extends Component{
+  state = {
     displayTab:0
   }
 
   toggleTabs(){
     if(this.state.displayTab === 0){
       return(
-        <div></div>
+        <div>
+        <hr/>
+        <h3>tab 1</h3>
+        </div>
       )
     }
     else if (this.state.displayTab === 1) {
       return(
-        <div></div>
+        <div>
+          <h3>hi</h3>
+        </div>
       )
     }
-    else if (this.state.displayTab === 2) {
-      return(
-        <div></div>
-      )
-
-    }
+    // else if (this.state.displayTab === 2) {
+    //   return(
+    //     <div></div>
+    //   )
+    //
+    // }
   }
   render(){
     return(
       <div>
-        <div className="container-grid">
-        <Tabs displayTab={this.state.displayTab} onClick={(tabId) => this.setState({displayTab:tabId})} ripple>
+        <div>
+        <Tabs displayTab={this.state.displayTab} onClick={(tabId) => this.setState({ displayTab:tabId })} ripple>
           <Tab>1</Tab>
           <Tab>1</Tab>
         </Tabs>
-
+        {this.toggleTabs()}
         </div>
       </div>
     )
   }
 }
-export default ActiveTab;
+export default ActiveTabs;
